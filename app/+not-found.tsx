df8d3adccd6e681ed81f3d43c1a5ce84c11a686d
Saurabh Negi <saurabhnegi.snn@gmail.com>
2025-02-03 18:30:08 +0530

    first commit expo app with tabs

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,32 +1,30 @@
-import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
-
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-
-export default function NotFoundScreen() {
-  return (
-    <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
-      <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen doesn't exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
-        </Link>
-      </ThemedView>
-    </>
-  );
-}
+import { Stack, Link } from "expo-router";
+import { Fragment } from "react";
+import { View, StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
+    backgroundColor: "#25292e",
+    justifyContent: "center",
+    alignItems: "center",
   },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
+  button: {
+    fontSize: 20,
+    textDecorationLine: "underline",
+    color: "#fff",
   },
 });
+
+export default function NotFoundScreen() {
+  return (
+    <Fragment>
+      <Stack.Screen options={{ title: "Oops! Not Found" }} />
+      <View style={styles.container}>
+        <Link href="/" style={styles.button}>
+          Go back to Home Screen!
+        </Link>
+      </View>
+    </Fragment>
+  );
+}
