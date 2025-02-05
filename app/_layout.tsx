ec87572189c806c84934e57e5edfbf98978bb7cd
Saurabh Negi <saurabhnegi.snn@gmail.com>
2025-02-05 11:47:29 +0530

    status bar, splash screen and icon

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,14 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { Fragment } from "react";
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" />
-    </Stack>
+    <Fragment>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <StatusBar style="light" />
+    </Fragment>
   );
 }
